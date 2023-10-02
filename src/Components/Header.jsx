ffb0562a7de2cf4a38e5df8290c1505567bef1c2
tito <olayiwola.titofunmi212@gmail.com>
2023-10-02 13:02:13 +0100

    routing update

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,16 +1,37 @@
+import { NavLink, useNavigate } from "react-router-dom";
+import { useState } from "react";
+
 const Header = () => {
+  const navigate = useNavigate();
+  const [toggleSideBar, setToggleSideBar] = useState(false);
+  const toggleMenu = () => {
+    setToggleSideBar(!toggleSideBar);
+  };
+
+  function signUpHandler() {
+    navigate("/Auth");
+  }
   return (
-    <header className='lg:px-[100px] py-[12px] shadow sticky top-0 bg-white'>
+    <header className="lg:px-[100px] py-[12px] shadow sticky top-0 bg-white">
       <nav className="flex items-center justify-between">
         <img src="/images/logo.svg" alt="logo" />
         <div className="flex gap-[39px]">
-          <span className='font-work font-medium'>Features</span>
-          <span className='font-work font-medium'>How It Works</span>
+          <li className="font-work font-medium" onClick={toggleMenu} id="features">
+            <a href="/#features" >Features</a>
+          </li>
+          <li className="font-work font-medium" onClick={toggleMenu} id="howItWorks">
+            <a href="#howItWorks">How It Works</a>
+          </li>
         </div>
-        <span className="font-sora font-semibold text-[18px] text-primary-main">Get Started</span>
+        <button
+          className="font-sora font-semibold text-[18px] text-primary-main"
+          onClick={signUpHandler}
+        >
+          Get Started
+        </button>
       </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/Pages/Recording.jsx
+++ src/Pages/Recording.jsx
@@ -1,9 +1,5 @@
-
-import React from 'react'
 import Header from '../Components/Header'
 import Footer from '../Components/Footer'
-// import { FaFacebook ,BiEditAlt} from 'react-icons/fa'
-// import videCard from '../Components/VideoCard'
 
 
 
@@ -13,11 +9,7 @@ const Recording = () => {
          <div className=' w-[90%] lg:w-[80%] mx-auto py-10'>
         </div>
          <Header />
-         
-         {/* <div className='w-full bg-[#e7e7ed] bg-opacity-20 py-10 mb-10'> */}
-
-         
-         
+ 
         <div className='max-w-[90%] lg:max-w-[500px] mx-auto text-center flex flex-col items-center gap-8'>
           <h5 className='text-[#141414] font-bold font-sora lg:text-xl'>
             To ensure the availability and privacy of your video, we recommend
@@ -28,9 +20,7 @@ const Recording = () => {
           </button>
           <p className='font-bold text-[#7e7e7e]'>
             Don’t have an account?{' '}
-            {/* <Link to='/auth' className='text-primary underline'> */}
               Create account{' '}
-            {/* </Link> */}
           </p>
         </div>
          
